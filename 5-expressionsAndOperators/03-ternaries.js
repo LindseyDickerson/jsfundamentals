621c0f8ac576426c8e17166307ffdf583c73642d
Lindsey Dickerson <lindseydickerson2@gmail.com>
2019-08-07 10:24:43 -0400

    test

--- 5-expressionsAndOperators/03-ternaries.js
+++ 5-expressionsAndOperators/03-ternaries.js
@@ -32,4 +32,5 @@ let age = 30;
 (age >= 25 && age <= 90) ? console.log('Yay! You can rent a car!')
  : (age >= 21) ? console.log('Yay! You can drink!')
  : (age >= 18) ? console.log('yay! you can vote!') 
- : (age <= 17) : console.log("Sorry, you're too young to do anything fun.");
+ : (age <= 17) ? console.log("Sorry, you're too young to do anything fun.")
+ : console.log("anything");
